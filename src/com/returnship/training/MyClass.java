e971575e08305e3ad6e433ac1d93ddb8232b4da3
mahen <mahen518@gmail.com>
2023-12-08 22:55:55 +0530

    initial commit

---
+++ src/com/returnship/training/MyClass.java
@@ -0,0 +1,15 @@
+package com.returnship.training;
+
+public class MyClass {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World from JAVA!");
+	}
+	
+}
+
+//git init
+// git add
+// git commit
+// add remote
+// git push
